3c8dd1344eb164dd1fe3887865ef0693334ce3c9
Riko-04 <echoge11@gmail.com>
2024-08-21 16:22:54 +0300

    Updates the API_BASE_URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:5000';
+const API_BASE_URL = 'https://greenthumbapp-2.onrender.com/';
 
 // Helper function to set the Authorization header
 function getAuthHeaders() {
